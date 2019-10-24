5ea4b04acf83479b1b1734808bf800653062ec20
m10721205@yuntech.edu.tw <ad1195380200>
2019-10-24 20:28:07 +0800

    2019/10/24: 1. 完成了code-review 2. 針對老師的建議做了修改:    2.1 修改無意義的註解(load_df函式)    2.2 在模型參數的部分補上註解(xgb, lgbm) 3. 自行針對整體進行檢視並補上註解 4. 修改xgb & lgbm的plot_importance中參數max_feature為32(原28)，圖片重新輸出

--- venv/GStore_DA.py
+++ venv/GStore_DA.py
@@ -4,48 +4,47 @@ import json  #JSON套件
 import numpy as np  #數學計算套件
 import pandas as pd  #python版的excel
 from pandas.io.json import json_normalize  #用python版的excel處理JSON
-import matplotlib.pyplot as plt  #畫圖的
+import matplotlib.pyplot as plt  #繪圖套件
 from sklearn import preprocessing  #sklearn的前處理套件
 from sklearn.metrics import mean_squared_error  #sklearn中評估指標: mse
 from sklearn.model_selection import train_test_split  #sklearn模型選擇套件中的訓練測試切割器
 from datetime import datetime  #日期時間套件
 import lightgbm as lgb  #LightGBM套件
 import xgboost as xgb  #XGBoost套件
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor  #決策樹套件
+from sklearn.ensemble import RandomForestRegressor  #隨機森林套件
 
 pd.options.mode.chained_assignment = None #鏈式賦值警告關閉
 pd.options.display.max_columns = 999      #最大顯示欄位數量
 
 #處理JSON欄位的方法: 分解巢狀欄位
-def load_df(csv_path="D:/EJ wang/google_data/train_v3.csv", nrows=None):
-    JSON_COLUMNS = ["device", "geoNetwork", "totals", "trafficSource"]
+def load_df(csv_path="D:/EJ wang/google_data/train_v3.csv", nrows=None):  #csv_path: data的絕對路徑, nrows: 載入的列樹
+    JSON_COLUMNS = ["device", "geoNetwork", "totals", "trafficSource"]  #自行設定JSON欄位名稱
     df = pd.read_csv(csv_path,
                      converters={column: json.loads for column in JSON_COLUMNS},
-                     dtype={"fullVisitorId": "str"},  # Important!!
+                     dtype={"fullVisitorId": "str"},  #因為fullVisitorId格式的緣故，必須轉成字串格式讓每個id都是唯一的
                      nrows=nrows)
-    for column in JSON_COLUMNS:
-        column_as_df = json_normalize(df[column])
-        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
-        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
-    print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
+    for column in JSON_COLUMNS:  #分解JSON
+        column_as_df = json_normalize(df[column])  #展開JSON欄位內的物件
+        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]  #強制規定格式, 用'.'來連接兩物件
+        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)  #展開完畢後刪除原本的JSON欄位
+    print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")  #印出data的資訊, 確認載入狀態
     return df
 
-#計算rmse
-def rmse(y_true, y_predict):
+def rmse(y_true, y_predict):  #計算rmse
     return round(np.sqrt(mean_squared_error(y_true, y_predict)), 3) #計算rmse取小數點後3位
 
-#LghtGBM模型建立
-def run_lgb(X_train, y_train, X_valid, y_valid, X_test, y_test):
+def run_lgb(X_train, y_train, X_valid, y_valid, X_test, y_test):  #LghtGBM模型建立
+    #X_train: 訓練集, y_train: 目標值(訓練集), X_valid: 驗證集, y_valid: 目標值(驗證集), X_test: 測試集, y_test: 目標值(測試集)
     params = {  #參數設定
-        "objective" : "regression",
-        "metric" : "rmse",
-        "max_depth": 6,
-        "num_leaves" : 50,
-        "learning_rate" : 0.005,
-        "bagging_fraction" : 0.75,
-        "feature_fraction" : 0.85,
-        "bagging_frequency" : 9,
+        "objective" : "regression",     #模型目標: 回歸
+        "metric" : "rmse",              #用RMSE評估模型
+        "max_depth": 6,                 #最大深度
+        "num_leaves" : 50,              #樹葉數量
+        "learning_rate" : 0.005,        #學習率
+        "bagging_fraction" : 0.75,      #採用樣本比例
+        "feature_fraction" : 0.85,      #採用特徵比例
+        "bagging_frequency" : 9,        #每9代的裝袋取樣頻率
         "verbosity" : -1
     }
     lgb_train = lgb.Dataset(X_train, label=y_train)  #訓練集設定
@@ -66,16 +65,16 @@ def run_lgb(X_train, y_train, X_valid, y_valid, X_test, y_test):
     print(f"LGBM:  RMSE train: {rmse(y_train, y_predict_train)}   RMSE val: {rmse(y_valid, y_predict_valid)}   RMSE test: {rmse(y_test, y_predict_test)}")
     return y_predict_test, model, evals_result  #回傳測試的y 績效、模型資訊、模型訓練後的資訊
 
-#XGBoost模型建立
-def run_xgb(X_train, y_train, X_val, y_val, X_test, y_test):
-    params = {'objective': 'reg:linear',  #參數設定
-              'eval_metric': 'rmse',
-              'eta': 0.01,
-              'max_depth': 8,
-              'subsample': 0.7,
-              'colsample_bytree': 0.7,
-              'alpha':0.001,
-              'silent': True}
+def run_xgb(X_train, y_train, X_val, y_val, X_test, y_test):  #XGBoost模型建立
+    #X_train: 訓練集, y_train: 目標值(訓練集), X_val: 驗證集, y_val: 目標值(驗證集), X_test: 測試集, y_test: 目標值(測試集)
+    params = {'objective': 'reg:linear',    #模型目標: 回歸
+              'eval_metric': 'rmse',        #用RMSE評估模型
+              'eta': 0.3,                   #學習率
+              'max_depth': 8,               #最大深度
+              'subsample': 0.7,             #採用樣本比例
+              'colsample_bytree': 0.7,      #採用特徵比例
+              'alpha':0.001,                #正規化系數
+              'silent': True}               #印出模型的執行資訊
     f_names = X_train.columns  #訓練集的欄位名稱
     xgb_train_data = xgb.DMatrix(np.asmatrix(X_train), y_train, feature_names=f_names)  #訓練集設定
     xgb_val_data = xgb.DMatrix(np.asmatrix(X_val), y_val, feature_names=f_names)  #驗證集設定
@@ -96,9 +95,9 @@ def run_xgb(X_train, y_train, X_val, y_val, X_test, y_test):
     print(f"XGBoost:  RMSE train: {rmse(y_train, y_predict_train)}   RMSE val: {rmse(y_val, y_predict_valid)}   RMSE test: {rmse(y_test, y_predict_test)}")
     return y_predict_test, model, evals_result  #回傳測試的y 績效、模型資訊、模型訓練後的資訊
 
-
-def run_tree(X_train, y_train, X_val, y_val, X_test, y_test, depth):
-    my_tree=DecisionTreeRegressor(max_depth=depth, max_features='auto')  #建立決策樹模型(參數: 最大深度)
+def run_tree(X_train, y_train, X_val, y_val, X_test, y_test, depth):  #決策樹模型建立
+    #X_train: 訓練集, y_train: 目標值(訓練集), X_val: 驗證集, y_val: 目標值(驗證集), X_test: 測試集, y_test: 目標值(測試集), depth: 樹木深度
+    my_tree=DecisionTreeRegressor(max_depth=depth, max_features='auto')  #建立決策樹模型(參數: 最大深度, 最大特徵數(自動))
     my_tree.fit(X_train, y_train)  #決策樹模型訓練
     y_predict_train = my_tree.predict(X_train)  #訓練的y 績效
     y_predict_valid = my_tree.predict(X_val)    #驗證的y 績效
@@ -106,9 +105,10 @@ def run_tree(X_train, y_train, X_val, y_val, X_test, y_test, depth):
     print(f"Decision tree: RMSE train: {rmse(y_train, y_predict_train)}  RMSE val: {rmse(y_val, y_predict_valid)}  RMSE test: {rmse(y_test, y_predict_test)}")
     return y_predict_test, my_tree
 
-
-def run_forest(X_train, y_train, X_val, y_val, X_test, y_test, depth, n_tree):
-    my_forest=RandomForestRegressor(max_depth=depth, n_estimators=n_tree, max_features='auto') #建立隨機森林模型(參數: 最大深度、決策樹數量)
+def run_forest(X_train, y_train, X_val, y_val, X_test, y_test, depth, n_tree):  #隨機森林模型建立
+    #X_train: 訓練集, y_train: 目標值(訓練集), X_val: 驗證集, y_val: 目標值(驗證集), X_test: 測試集, y_test: 目標值(測試集)
+    #depth: 樹木深度, n_tree: 決策樹數量
+    my_forest=RandomForestRegressor(max_depth=depth, n_estimators=n_tree, max_features='auto') #建立隨機森林模型(參數: 最大深度, 決策樹數量, 最大特徵數(自動))
     my_forest.fit(X_train, y_train)  #隨機森林模型訓練
     y_predict_train = my_forest.predict(X_train)  #訓練的y 績效
     y_predict_valid = my_forest.predict(X_val)    #驗證的y 績效
@@ -149,24 +149,21 @@ cat_cols = ["channelGrouping", "device.browser", "device.deviceCategory", "devic
             "trafficSource.adwordsClickInfo.page", "trafficSource.adwordsClickInfo.slot", "trafficSource.campaign",
             "trafficSource.keyword", "trafficSource.medium", "trafficSource.referralPath", "trafficSource.source",
             'trafficSource.adwordsClickInfo.isVideoAd', 'trafficSource.isTrueDirect']
-for col in cat_cols:  #for迴圈
+for col in cat_cols:  #將類別型資料一起用lbl編碼器編碼
     lbl = preprocessing.LabelEncoder()  #建立label編碼器物件
     lbl.fit(list(train_df[col].values.astype('str')) + list(test_df[col].values.astype('str')))  #類別特徵轉換成1, 2, 3, ...
     train_df[col] = lbl.transform(list(train_df[col].values.astype('str')))
     test_df[col] = lbl.transform(list(test_df[col].values.astype('str')))
     print(col)
 
-##將數值型資料型態轉換成float
 num_cols = ["totals.hits", "totals.pageviews", "visitNumber", 'totals.bounces',  'totals.newVisits',]  #數值型特徵陣列
             # 'totals.sessionQualityDim', 'totals.timeOnSite',
             # 'totals.transactions']
-for col in num_cols:  #用for將數值型欄位型態轉float
+for col in num_cols:  #將數值型欄位型態轉float
     train_df[col] = train_df[col].astype(float)
     test_df[col] = test_df[col].astype(float)
     print(col)
 
-##訓練 / 驗證 / 測試 資料拆分
-   ##method_1
 X=train_df.drop(['date', 'fullVisitorId', 'visitId', 'visitStartTime', 'totals.transactionRevenue'], axis=1)   ## X:資料集
 num_cols_2 = ['totals.sessionQualityDim', 'totals.timeOnSite',   ## 數值型態欄位
             'totals.transactions']
@@ -178,7 +175,7 @@ for col in num_cols_2:   ## 數值型態轉換float
 y = train_df["totals.transactionRevenue"].values.astype(float) / 1000000   ##訓練資料目標值型態轉float以及還原真實金額(除以10^6)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size= 0.3, random_state=11)   ## 訓練7 / 驗證3 資料拆分
 
-testRange = test_df[test_df['date']<=datetime(2018,7,16)]   ##測試資料範圍縮小為約16萬 (原始資料約40幾萬)
+testRange = test_df[test_df['date']<=datetime(2018,7,16)]   ##測試資料範圍縮小為約20萬筆 (原始資料約40幾萬)
 X_test = testRange.drop(['date', 'fullVisitorId', 'visitId', 'visitStartTime', 'totals.transactionRevenue'], axis=1)  ##測試資料集3版
 y_test = testRange['totals.transactionRevenue'].values.astype(float)/1000000   ###測試資料的目標值
 
@@ -208,13 +205,13 @@ ax = lgb.plot_metric(lgb_evals_result, metric='rmse')
 plt.title('LightGBM Metric during training (learning rate: 0.01)')
 plt.show()
     ##XGB##
-epochs = len(xgb_evals_result['train']['rmse'])
-x_axis = range(0, epochs)
-fig, ax = plt.subplots()
-ax.plot(x_axis, xgb_evals_result['train']['rmse'], label='Train')
-ax.plot(x_axis, xgb_evals_result['valid']['rmse'], label='Valid')
+epochs = len(xgb_evals_result['train']['rmse'])  #取出迭代次數
+x_axis = range(0, epochs)  #設定x軸的範圍 0~epochs
+fig, ax = plt.subplots()   #開始畫圖
+ax.plot(x_axis, xgb_evals_result['train']['rmse'], label='Train')  #畫訓練集的迭代曲線
+ax.plot(x_axis, xgb_evals_result['valid']['rmse'], label='Valid')  #畫驗證集的迭代曲線
 ax.legend()
-# plt.xlabel('iterations')
+plt.xlabel('iterations')
 plt.ylabel('rmse')
 plt.title('XGBoost Metric during training (learning rate: 0.01)')
 plt.grid(True)
@@ -222,11 +219,11 @@ plt.show()
 
 #畫importance
     ##LGBM##
-ax = lgb.plot_importance(lgb_model, max_num_features=28)
+ax = lgb.plot_importance(lgb_model, max_num_features=32)
 plt.title('LightGBM Feature Importance (learning rate: 0.01, best iteration: 200)')
 plt.show()
     ##XGB##
-ax = xgb.plot_importance(xgb_model, max_num_features=28)
+ax = xgb.plot_importance(xgb_model, max_num_features=32)
 plt.title('XGBoost Feature Importance (learning rate: 0.01, best iteration: 100)')
 plt.show()
     ##Decision Tree/RandomForest
